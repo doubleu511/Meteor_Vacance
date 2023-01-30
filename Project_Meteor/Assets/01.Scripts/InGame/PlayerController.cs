3eb0b6851742831b9458ff1b1d74a9159f9e1e8b
Junhyup Lee <junhyup511@naver.com>
2023-01-31 02:56:48 +0900

    애니메이션 플로우 추가 및 사정거리 고민완료

--- Project_Meteor/Assets/01.Scripts/InGame/PlayerController.cs
+++ Project_Meteor/Assets/01.Scripts/InGame/PlayerController.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] Animator playerAnimator;
+
+    private Vector2 lookDir = Vector2.right;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision);
+    }
 }
