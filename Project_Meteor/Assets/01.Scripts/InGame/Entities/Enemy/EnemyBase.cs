782684922778670a77a0cc83b02ca96808e23c96
Junhyup Lee <64317456+doubleu511@users.noreply.github.com>
2023-02-03 16:48:46 +0900

    적관련 기본 베이스 추가

---
+++ Project_Meteor/Assets/01.Scripts/InGame/Entities/Enemy/EnemyBase.cs
@@ -0,0 +1,58 @@
+using DG.Tweening.Core.Easing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer sprite;
+    [SerializeField] private float flashTime = 0f;
+    Color originColor = Color.white;
+
+    protected string myName;
+    public float moveSpeed = 0f;
+    public float damage = 3f;
+
+    public float aliveTime = 0f;
+    public float movedDistance = 0f;
+
+    private int targetWayPointIndex = 0;
+
+    private HealthSystem healthSystem;
+
+    private void Awake()
+    {
+        healthSystem = GetComponent<HealthSystem>();
+    }
+
+    protected virtual void Start()
+    {
+        aliveTime = 0f;
+    }
+
+    // µ¥¹ÌÁö ÀÔ¾úÀ» ¶§ ±ôºý±ôºý Ã³¸®
+    public void EnemyFlashStart()
+    {
+        sprite.color = Color.red;
+        Invoke("EnemyFlashStop", flashTime);
+    }
+
+    void EnemyFlashStop()
+    {
+        sprite.color = originColor;
+    }
+
+    protected virtual void Update()
+    {
+        aliveTime += Time.deltaTime;
+        movedDistance = aliveTime * moveSpeed;
+    }
+
+    public void WaveStatControl(int wave)
+    {
+        float value_f = (wave * Mathf.Pow(1.5f, 0)) * 100;
+        int value = (int)value_f;
+
+        healthSystem.SetHealthAmountMax(value, true); // Ã¼·Â Á¶Àý
+    }
+}
